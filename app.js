0182ab35ddce11db1a5dcadee9e0842519c0d003
adam krawiec <a.krawiec@exlabs.co.uk>
2023-08-03 22:27:46 +0200

    Adds gitignore and basic app setup

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require("cors");
+
+const PORT = 3000;
+const app = express();
+
+const corsOptions = {
+  origin: "http://localhost:3000"
+};
+
+app.use(bodyParser.json());
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hello there"
+  })
+})
+
+app.listen(PORT, () => {
+  console.log(`[server]: Server is running on a port ${PORT}`)
+})
