feea3b75b1b7367b89efaa28e62a47d340687865
adam krawiec <a.krawiec@exlabs.co.uk>
2023-08-03 22:50:08 +0200

    Adds home controller

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require("cors");
 const db = require("./db");
 
+const homeController = require("./home.controller");
 const PORT = 3000;
 const app = express();
 
@@ -10,23 +11,17 @@ const corsOptions = {
   origin: "http://localhost:3000"
 };
 
+db.sequelize
+  .sync()
+  .then(() => console.log("Synced db.") )
+  .catch((err) => console.log("Failed to sync db: " + err.message));
+
 app.use(bodyParser.json());
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.json({
-    message: "Hello there"
-  })
-})
-
-db.sequelize
-  .sync()
-  .then(() => console.log("Synced db.") )
-  .catch((err) => {
-    console.log("Failed to sync db: " + err.message);
-  });
+app.use("/", homeController);
 
 app.listen(PORT, () => {
   console.log(`[server]: Server is running on a port ${PORT}`)
---
+++ home.controller.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+router.use((req, res, next) => {
+  console.log(`Time: ${Date.now()} - ${req.method}: ${req.path}`)
+  next()
+})
+
+router.get("/", (req, res) => {
+  res.json({
+    message: "Hello there"
+  });
+})
+
+module.exports = router;
