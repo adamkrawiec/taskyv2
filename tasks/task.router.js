7bc4b63d7077dec6cbd998117f7e72c5ddb6816d
adam krawiec <a.krawiec@exlabs.co.uk>
2023-09-07 13:02:57 +0200

    Adds more methods to tasks controller

--- tasks/task.router.js
+++ tasks/task.router.js
@@ -6,30 +6,10 @@ const taskDTO = require("./task.dto");
 const tasksController = require("./tasks.controller");
 const router = express.Router();
 
-router.get("/", async (req, res) => {
-  let tasks = await Task.findAll( { include: User });
-  tasks = tasks.map((task) => taskDTO(task));
+router.get("/", tasksController.index);
+router.get("/:id", tasksController.showById);
+router.get("/user/:userId", tasksController.showByUserId);
 
-  const title = req.t('tasks.index.title')
-  res.json({ title, data: tasks });
-});
-
-router.get("/:id", async (req, res) => {
-  const task = await Task.findByPk(req.params.id);
-  const taskdto = taskDTO(task);
-
-  res.json({ data: task });
-});
-
-router.post("/", tasksController.create)
-
-router.get("/user/:userId", async (req, res) => {
-  const user = await User.findByPk(req.params.userId);
-
-  let tasks = await user.getTasks({ include: User });
-  tasks = tasks.map((task) => taskDTO(task));
-
-  res.json({ data: tasks });
-})
+router.post("/", tasksController.create);
 
 module.exports = router;
--- tasks/tasks.controller.js
+++ tasks/tasks.controller.js
@@ -2,6 +2,30 @@ const Task = require("./task.model");
 const User = require("../users/user.model");
 const TaskMailer = require("./task.mailer");
 
+const index = async (req, res) => {
+  let tasks = await Task.findAll( { include: User });
+  tasks = tasks.map((task) => taskDTO(task));
+
+  const title = req.t('tasks.index.title')
+  res.json({ title, data: tasks });
+};
+
+const showById = async (req, res) => {
+  const task = await Task.findByPk(req.params.id);
+  const taskdto = taskDTO(task);
+
+  res.json({ data: task });
+};
+
+const showByUserId = async (req, res) => {
+  const user = await User.findByPk(req.params.userId);
+
+  let tasks = await user.getTasks({ include: User });
+  tasks = tasks.map((task) => taskDTO(task));
+
+  res.json({ data: tasks });
+};
+
 const create = async (req, res) => {
   const deadlineAt = req.body.deadline && new Date(req.body.deadline);
   const user = await User.findByPk(req.body.user_id);
@@ -24,4 +48,6 @@ const create = async (req, res) => {
 
 module.exports = {
   create,
+  index,
+  showById,
 }
