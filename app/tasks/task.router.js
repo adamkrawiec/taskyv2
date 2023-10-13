fe97d8334d7850bb483500d8a62d8b88e69f554a
adam krawiec <a.krawiec@exlabs.co.uk>
2023-10-13 23:46:42 +0200

    adds seed files to generate some fake data

--- app/tasks/task.router.js
+++ app/tasks/task.router.js
@@ -7,14 +7,14 @@ const router = express.Router();
 router.route("/").get(tasksController.index)
                  .post(tasksController.create);
 
+router.get("/my", tasksController.myTasks);
+router.get("/summary", tasksController.summary);
+router.get("/user/:userId", tasksController.showByUserId);
+
 router.route("/:id").get(tasksController.showById)
                     .put(tasksController.update)
                     .delete(tasksController.destroy);
 
 router.patch("/:id/complete", tasksController.complete);
-
-router.get("/my", tasksController.myTasks);
-router.get("/summary", tasksController.summary);
-router.get("/user/:userId", tasksController.showByUserId);
 
 module.exports = router;
