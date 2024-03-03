de127877903db25631d5f1923a8e168b23e1a7ad
adam krawiec <a.krawiec@exlabs.co.uk>
2024-03-03 23:24:37 +0100

    Adds backbone for permitted params middleware

---
+++ middlewares/permitParams.js
@@ -0,0 +1,8 @@
+// make it work with joy schema
+const permitParams = (schema) => (req, res, next) => {
+  // if schema valid return next
+
+  // return validation errors
+};
+
+module.exports = permitParams;
